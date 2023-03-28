f30228f51171827786705e3a7438e0336649d3e0
Simón Sánchez Maya <119452910+SimonTry@users.noreply.github.com>
2023-03-27 23:22:52 -0500

    init project
    init project

---
+++ index.js
@@ -0,0 +1,27 @@
+var message = new SpeechSynthesisUtterance($("#text").val());
+var voices = speechSynthesis.getVoices();
+
+$("input").on("change", function () {
+    console.log($(this).attr("id"), $(this).val());
+    message[$(this).attr("id")] = $(this).val();
+});
+
+$("select").on("change", function () {
+    message.voice = voices[$(this).val()];
+    var valor = $(this).val();
+    console.log(valor);
+});
+
+$("button").on("click", function () {
+    speechSynthesis.speak(message);
+});
+
+// Hack around voices bug
+var interval = setInterval(function () {
+    voices = speechSynthesis.getVoices();
+    if (voices.length) clearInterval(interval); else return;
+
+    for (var i = 0; i < voices.length; i++) {
+        $("select").append("<option value=\"" + i + "\">" + voices[i].name + "</option>");
+    }
+}, 10);
