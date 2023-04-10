80431f4aa70eed7e9c1088e66a15f0eeed8296c4
Simón Sánchez Maya <119452910+SimonTry@users.noreply.github.com>
2023-04-10 10:10:03 -0500

    Actualizacion de guia del index js
    Se agregan comentarios necesarios para enteder la logica del index.ja para el uso del api de speech

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+//inicializamos la clase SpeechSynthesisUtterance
 var message = new SpeechSynthesisUtterance($("#text").val());
+//obtenemos las voces disponibles del api
 var voices = speechSynthesis.getVoices();
-
+//se captura el mensaje agregado en el input
 $("input").on("change", function () {
     console.log($(this).attr("id"), $(this).val());
     message[$(this).attr("id")] = $(this).val();
 });
-
+//se setea el valor de la voz seleccionada y se imprime en consola
 $("select").on("change", function () {
     message.voice = voices[$(this).val()];
     var valor = $(this).val();
     console.log(valor);
 });
-
+//evento que acciona la funcion de reproducir el texto en voz
 $("button").on("click", function () {
     speechSynthesis.speak(message);
 });
